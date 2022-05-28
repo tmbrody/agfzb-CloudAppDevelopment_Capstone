6702d3011b4ef5289518937334daabd6f2624837
Rolly <brodytaylor311@gmail.com>
2022-05-28 04:39:09 +0000

    First commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,19 +8,19 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
-    # path for about view
-
-    # path for contact us view
-
-    # path for registration
-
-    # path for login
-
-    # path for logout
-
     path(route='', view=views.get_dealerships, name='index'),
 
+    path('index/', views.index, name='index'),
+    # path for about view
+    path('about/', views.about, name='about'),
+    # path for contact us view
+    path('contact/', views.contact, name='contact'),
+    # path for registration
+    path('registration/', views.registration_request, name='registration'),
+    # path for login
+    path('login/', views.login_request, name='login'),
+    # path for logout
+    path('logout/', views.logout_request, name='logout'),
     # path for dealer reviews view
 
     # path for add a review view
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "capstone-django-app.mybluemix.net"]
 
 
 # Application definition
