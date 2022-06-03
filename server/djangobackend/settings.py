14d3b5b6e6b75317ad3805cb96215a99348ef3aa
Rolly Tourisman <brodytaylor311@gmail.com>
2022-06-03 01:42:09 +0000

    Update settings.py

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["capstone-django-app.us-south.mybluemix.net", "localhost"]
-
+ALLOWED_HOSTS = ["localhost", "localhost:8000", "capstone-django-app.us-south.mybluemix.net", "https://onesockboy31-8000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+CSRF_TRUSTED_ORIGINS = ['https://onesockboy31-8000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 # Application definition
 
